d00da3a49e3df197c955a1af73529e52c17b298d
itc-jason <jason.chen@it-consultis.net>
2023-08-04 14:04:45 +0800

    sixth change

--- src/app/database/index.ts
+++ src/app/database/index.ts
@@ -1,5 +1,17 @@
 import mysql from "mysql2/promise";
+import fs from 'fs';
 
-const db = mysql.createPool(process.env.DATABASE_URL as string);
+const config = {
+    host: process.env.PLANETSCALE_DB_HOST as string,
+    user: process.env.PLANETSCALE_DB_USERNAME as string,
+    password:  process.env.PLANETSCALE_DB_PASSWORD as string,
+    port:3306,
+    ssl: {
+      ca: fs.readFileSync(process.env.PLANETSCALE_SSL_CERT_PATH as string), 
+    },
+};
+
+
+const db = mysql.createPool(config);
 
 export default db;
